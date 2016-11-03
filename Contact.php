27c11a29414d1417cbe159d14d2c047fe1fffce6
daveNienhuis <dave.nienhuis@hotmail.com>
2016-11-03 11:30:51 +0100

    Main bestanden

---
+++ Contact.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact</title>
+    <link rel="stylesheet" type="text/css" href="styles.css"/>
+</head>
+
+<body>
+
+
+<div class = "mainContainer">
+
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+    <a href="Homepage.php">Home     </a>&nbsp;
+    <a href="Inloggen.php">Inloggen </a>&nbsp;
+    <a href="Contact.php">Contact   </a>&nbsp;
+    <a href="Recensie.php">Recensie </a>&nbsp;
+
+    <img src="Kabeltrans.png" style="float: left;"> <br /><br /><br /><br /><br /><br /><br />
+
+    <li><a href="bar/catagorie.php">Catagorie</a>           </li>
+    <li><a href="bar/audiokabel.php">Audiokabels</a>        </li>
+    <li><a href="bar/tvkabel.php">TV-Kabels</a>             </li>
+    <li><a href="bar/netwerkkabel.php"">Netwerkkabels </a>  </li>
+    <li><a href="bar/verlengsnoer.php">Verlengsnoeren</a>   </li>
+    <li><a href="bar/hdmikabel.php">HDMI-Kabels</a>         </li>
+    <li><a href="bar/smartphone.php">Smartphone/tablet</a>  </li>
+    <li><a href="bar/usbkabel.php">USB-Kabels</a>           </li>
+    <li><a href="bar/spelcomputer.php">Spelcomputer</a>     </li>
+    <li><a href="bar/vastetelefoon.php">Vaste telefonie</a> </li>
+    <li><a href="bar/fotoencamera.php">Foto en Camera</a>   </li>
+
+    <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
+
+
+</body>
+</html>
+
