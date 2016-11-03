98bac9f469657172383061f74bb1f6d3c50964b0
daveNienhuis <dave.nienhuis@hotmail.com>
2016-11-03 21:51:40 +0100

    Add files via upload
    a href's aangepast naar bar_v2, dit was nog niet gedaan binnen de main pagina's.

--- Contact.php
+++ Contact.php
@@ -25,17 +25,17 @@
 
     <img src="Kabeltrans.png" style="float: left;"> <br /><br /><br /><br /><br /><br /><br />
 
-    <li><a href="bar/catagorie.php">Catagorie</a>           </li>
-    <li><a href="bar/audiokabel.php">Audiokabels</a>        </li>
-    <li><a href="bar/tvkabel.php">TV-Kabels</a>             </li>
-    <li><a href="bar/netwerkkabel.php"">Netwerkkabels </a>  </li>
-    <li><a href="bar/verlengsnoer.php">Verlengsnoeren</a>   </li>
-    <li><a href="bar/hdmikabel.php">HDMI-Kabels</a>         </li>
-    <li><a href="bar/smartphone.php">Smartphone/tablet</a>  </li>
-    <li><a href="bar/usbkabel.php">USB-Kabels</a>           </li>
-    <li><a href="bar/spelcomputer.php">Spelcomputer</a>     </li>
-    <li><a href="bar/vastetelefoon.php">Vaste telefonie</a> </li>
-    <li><a href="bar/fotoencamera.php">Foto en Camera</a>   </li>
+    <li><a href="bar_v2/catagorie.php">Catagorie</a>           </li>
+    <li><a href="bar_v2/audiokabel.php">Audiokabels</a>        </li>
+    <li><a href="bar_v2/tvkabel.php">TV-Kabels</a>             </li>
+    <li><a href="bar_v2/netwerkkabel.php"">Netwerkkabels </a>  </li>
+    <li><a href="bar_v2/verlengsnoer.php">Verlengsnoeren</a>   </li>
+    <li><a href="bar_v2/hdmikabel.php">HDMI-Kabels</a>         </li>
+    <li><a href="bar_v2/smartphone.php">Smartphone/tablet</a>  </li>
+    <li><a href="bar_v2/usbkabel.php">USB-Kabels</a>           </li>
+    <li><a href="bar_v2/spelcomputer.php">Spelcomputer</a>     </li>
+    <li><a href="bar_v2/vastetelefoon.php">Vaste telefonie</a> </li>
+    <li><a href="bar_v2/fotoencamera.php">Foto en Camera</a>   </li>
 
     <br/><br/><br/><br/><br/><br/><br/><br/><br/>
 
