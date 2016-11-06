48a51099021c1d99f4b9dafe236e051e73d65766
daveNienhuis <dave.nienhuis@hotmail.com>
2016-11-06 19:01:44 +0100

    Add files via upload
    Side menu aangepast, php inlog testen.

--- Homepage.php
+++ Homepage.php
@@ -3,6 +3,7 @@
 <head>
     <title>Homepage</title>
     <link rel="stylesheet" type="text/css" href="styles.css"/>
+
 </head>
 
 <body>
@@ -18,26 +19,31 @@
     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
-    <a href="Homepage.php">Home     </a>&nbsp;
-    <a href="Inloggen.php">Inloggen </a>&nbsp;
-    <a href="Contact.php">Contact   </a>&nbsp;
-    <a href="Recensie.php">Recensie </a>&nbsp;
+    <a class="btn" href="Homepage.php">Home     </a>&nbsp;
+    <a class="btn" href="Inloggen.php">Inloggen </a>&nbsp;
+    <a class="btn" href="Contact.php">Contact   </a>&nbsp;
+    <a class="btn" href="Recensie.php">Recensie </a>&nbsp;
 
     <img src="Kabeltrans.png" style="float: left;"> <br /><br /><br /><br /><br /><br /><br />
 
-    <li><a href="bar_v2/catagorie.php">Catagorie</a>           </li>
-    <li><a href="bar_v2/audiokabel.php">Audiokabels</a>        </li>
-    <li><a href="bar_v2/tvkabel.php">TV-Kabels</a>             </li>
-    <li><a href="bar_v2/netwerkkabel.php"">Netwerkkabels </a>  </li>
-    <li><a href="bar_v2/verlengsnoer.php">Verlengsnoeren</a>   </li>
-    <li><a href="bar_v2/hdmikabel.php">HDMI-Kabels</a>         </li>
-    <li><a href="bar_v2/smartphone.php">Smartphone/tablet</a>  </li>
-    <li><a href="bar_v2/usbkabel.php">USB-Kabels</a>           </li>
-    <li><a href="bar_v2/spelcomputer.php">Spelcomputer</a>     </li>
-    <li><a href="bar_v2/vastetelefoon.php">Vaste telefonie</a> </li>
-    <li><a href="bar_v2/fotoencamera.php">Foto en Camera</a>   </li>
 
-    <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
+    <ul>
+        <li><a href="bar_v2/catagorie.php">Catagorie</a></li>
+        <li><a href="bar_v2/audiokabel.php">Audiokabels</a></li>
+        <li><a href="bar_v2/tvkabel.php">TvKabels</a></li>
+        <li><a href="bar_v2/netwerkkabel.php">Netwerkkabels</a></li>
+        <li><a href="bar_v2/verlengsnoer.php">Verlengsnoeren</a></li>
+        <li><a href="bar_v2/hdmikabel.php">HDMIkabels</a></li>
+        <li><a href="bar_v2/smartphone.php">Smartphones</a></li>
+        <li><a href="bar_v2/usbkabel.php">USBkabels</a></li>
+        <li><a href="bar_v2/spelcomputer.php">Spelcomputers</a></li>
+        <li><a href="bar_v2/vastetelefoon.php">Vaste telefonie</a></li>
+        <li><a href="bar_v2/fotoencamera.php">Foto en Camera</a></li>
+    </ul>
+
+
+
 
 
 
