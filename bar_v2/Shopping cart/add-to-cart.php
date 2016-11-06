58ffcac54a2b813c2bc77ae21e9cb61ea84e1bf5
daveNienhuis <dave.nienhuis@hotmail.com>
2016-11-06 19:54:41 +0100

    Add files via upload

---
+++ bar_v2/Shopping cart/add-to-cart.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dave
+ * Date: 03/11/2016
+ * Time: 21:58
+ */
--- bar_v2/vastetelefoon.php
+++ bar_v2/vastetelefoon.php
@@ -31,17 +31,19 @@ img {
 
     <img src="../Kabeltrans.png" style="float: left;" /> <br /><br /><br /><br /><br /><br /><br />
 
-    <li><a href="catagorie.php">Catagorie</a></li>
-    <li><a href="audiokabel.php">Audiokabels</a></li>
-    <li><a href="tvkabel.php">TV-Kabels</a></li>
-    <li><a href="netwerkkabel.php"">Netwerkkabels </a></li>
-    <li><a href="verlengsnoer.php">Verlengsnoeren</a></li>
-    <li><a href="hdmikabel.php">HDMI-Kabels</a></li>
-    <li><a href="smartphone.php">Smartphone/tablet</a></li>
-    <li><a href="usbkabel.php">USB-Kabels</a></li>
-    <li><a href="spelcomputer.php">Spelcomputer</a></li>
-    <li><a href="vastetelefoon.php">Vaste telefonie</a></li>
-    <li><a href="fotoencamera.php">Foto en Camera</a></li>
+    <ul>
+        <li><a href="catagorie.php">Catagorie</a></li>
+        <li><a href="audiokabel.php">Audiokabels</a></li>
+        <li><a href="tvkabel.php">TvKabels</a></li>
+        <li><a href="netwerkkabel.php">Netwerkkabels</a></li>
+        <li><a href="verlengsnoer.php">Verlengsnoeren</a></li>
+        <li><a href="hdmikabel.php">HDMIkabels</a></li>
+        <li><a href="smartphone.php">Smartphones</a></li>
+        <li><a href="usbkabel.php">USBkabels</a></li>
+        <li><a href="spelcomputer.php">Spelcomputers</a></li>
+        <li><a href="vastetelefoon.php">Vaste telefonie</a></li>
+        <li><a href="fotoencamera.php">Foto en Camera</a></li>
+    </ul>
 
      <center> 
 <h1>Telefonie kabels</h1>
