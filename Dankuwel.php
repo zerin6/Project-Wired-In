d613fce6c03f9d845e3f1c3d117c8346f1957da2
samvanputten95 <s.van.putten@st.hanze.nl>
2016-11-10 12:48:39 +0100

    Add files via upload

---
+++ Dankuwel.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+require('dbh.php');
+
+$firstname = $_POST['Voornaam'];
+$lastname = $_POST['Achternaam'];
+$email = $_POST['Emailadres'];
+$subject = $_POST['Onderwerp'];
+$message = $_POST['Bericht'];
+
+$sql = "INSERT into contactformulieren (Voornaam, Achternaam, Emailadres, Onderwerp, Bericht)
+VALUES ('$firstname','$lastname','$email','$subject','$message')";
+
+$result = mysqli_query($conn, $sql);
+header( "refresh:3;url=Contact.php" );
+?>
+<html>
+<h4>Dank voor uw bericht. Wij zullen zo spoedig mogelijk contact opnemen.</h4>
+</html>
+
+
+
